0cb2f25fa4f28b190cf4bfde7809aa233e1ded7e
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-07-10 20:56:46 +0530

    fisr commit

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const data = [
+        ["Project 1", "HTML, CSS, JavaScript", './project1/index.html'],
+        ["Project 2", "HTML, CSS, React", './project2/index.html'],
+        ["Project 3", "HTML, CSS, Node.js", './project3/index.html']
+    ];
+
+    const tableBody = document.querySelector('#data-table tbody');
+
+    data.forEach(rowData => {
+        const row = document.createElement('tr');
+
+        rowData.forEach((cellData, index) => {
+            const cell = document.createElement('td');
+
+            if (index === 2) {  // Assuming the 3rd item in the array is the URL
+                const link = document.createElement('a');
+                link.href = cellData;
+                link.textContent = "View Project";  // Text for the link
+                link.target = "_blank"; // Open link in a new tab
+                cell.appendChild(link);
+                cell.setAttribute('data-label', 'Link');
+            } else {
+                cell.textContent = cellData;
+                cell.setAttribute('data-label', ['Projects Name', 'What is Using'][index]);
+            }
+            row.appendChild(cell);
+        });
+
+        tableBody.appendChild(row);
+    });
+});
